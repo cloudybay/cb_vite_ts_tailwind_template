3420be4abb8b8d7e3d6fd3000f59d1943b10612b
jocelyn20618 <jocelyn20618@gmail.com>
2024-10-15 10:50:51 +0800

    change assets_require to normal function

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,11 +1,3 @@
-export {}
-
-declare global {
-  interface Window {
-    assets_require(path: string): string
-  }
-}
-
-window.assets_require = (path: string) => {
+export const assets_require = (path: string) => {
   return new URL(`/src/assets/${path}`, import.meta.url).href
 }
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import router from './router'
 import "./assets/css/style.css"
 // import "./font_icon/style.css"
 // import './libs/cb-js/cb.date.ts'
-import './libs/utils.ts'
 
 const pinia = createPinia()
 const app = createApp(App)
